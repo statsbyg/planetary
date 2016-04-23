243ba8eed4ef7618d41acce2a333a560eb36434a
Yonathan Redda (yredda) <yredda@cisco.com>
2016-04-23 09:25:58 -0700

    added two of the closest planets to the sun

--- pproj/app/src/main/java/paxt/travis/com/model/Planet.java
+++ pproj/app/src/main/java/paxt/travis/com/model/Planet.java
@@ -4,4 +4,8 @@ package paxt.travis.com.model;
  * Created by yredda on 4/23/16.
  */
 public class Planet {
+
+    private static final String MERCURY = "MERCURY - the most closest planet to the sun";
+    private static final String VENUS = "VENUS - the second closest planet to the sun";
+
 }
