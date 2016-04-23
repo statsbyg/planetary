ea5347b76424fd342f250e4aa1e0844dc8147c9c
Yonathan Redda (yredda) <yredda@cisco.com>
2016-04-23 09:54:04 -0700

    added the that last two planetary objects in our solar system

--- pproj/app/src/main/java/paxt/travis/com/model/Planet.java
+++ pproj/app/src/main/java/paxt/travis/com/model/Planet.java
@@ -4,4 +4,7 @@ package paxt.travis.com.model;
  * Created by yredda on 4/23/16.
  */
 public class Planet {
+
+    private static final String URANUS = "URANUS - The farthest planet from the sun";
+    private static final String PLUTO = "PLUTO - The farther and only dwarf planet from the sun";
 }
